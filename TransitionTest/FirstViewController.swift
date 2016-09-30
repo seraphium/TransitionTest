b61dde9ff9c21dd73477830a2c711b9a35fce076
seraphium <viperking@163.com>
2016-09-30 09:53:36 +0800

    first commit on transition test pro

---
+++ TransitionTest/FirstViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.swift
+//  TransitionTest
+//
+//  Created by Jackie Zhang on 16/9/29.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController : UIViewController {
+    
+    
+}
---
+++ TransitionTest/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  TransitionTest
+//
+//  Created by Jackie Zhang on 16/9/29.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController : UIViewController {
+    
+    
+    @IBAction func dismissClicked(_ sender: UIButton)
+    {
+        
+        
+    }
+}
